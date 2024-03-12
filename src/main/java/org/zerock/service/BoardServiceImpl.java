2a0c8388f8ad7932a92099b1a628c1ad79b68107
leehana <lubihana12@gmail.com>
2024-03-12 11:07:35 +0900

    BoardServiceImplTest 클래스 생성 후 Exist,Register test완료

--- src/main/java/org/zerock/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -1,5 +1,57 @@
 package org.zerock.service;
 
-public class BoardServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+//인터페이스는 구현체 - implements가 붙으면 인터페이스가 붙었다는 의미 - 구현체 (Override)
+@Log4j
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService{
+
+  
+   // spring 4.3 이상에서 자동 처리
+  @Autowired
+    private BoardMapper mapper;
+   
+  
+  @Override
+  public void register(BoardVO board) {
+    
+    log.info("register....." + board);
+    
+    mapper.insertSelectKey(board);
+  }
+
+  @Override
+  public BoardVO get(Long bno) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean modify(BoardVO board) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean remove(BoardVO board) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public List<BoardVO> getList() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
 }
---
+++ src/test/java/org/zerock/service/BoardServiceImplTest.java
@@ -0,0 +1,43 @@
+package org.zerock.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardServiceImplTest {
+
+  @Autowired
+  private BoardService service;
+  
+  @Test
+  public void testExist() {
+    
+    log.info(service);
+    assertNotNull(service); //존재유무 체크
+  }
+
+  @Test
+  public void testRegister() {
+    
+    BoardVO board = new BoardVO();
+    board.setTitle("새로 작성하는 글- ser");
+    board.setContent("새로 작성하는 글 내용 -ser");
+    board.setWriter("newbie - ser");
+    
+    service.register(board);
+    
+    log.info("생성된 게시물 번호 : " + board.getBno());
+  }
+  
+  
+}
